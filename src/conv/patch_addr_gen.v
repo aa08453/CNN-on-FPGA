11a3310d48c9a612c2db09783a85f48fe3471a8f
aa08453 <aa08453@st.habib.edu.pk>
2025-07-26 12:01:40 +0500

    some fixes in patch logic

--- src/conv/patch_addr_gen.v
+++ src/conv/patch_addr_gen.v
@@ -27,10 +27,8 @@ module patch_addr_gen
     reg [4:0] prev_i;
 
 
-
     always @(posedge clk or negedge rst) 
     begin
-//        integer mv, nv;
         if (!rst) 
         begin
             prev_i <= 5'd28;  // Something invalid 
@@ -51,12 +49,10 @@ module patch_addr_gen
             if (IC == 0)
             begin
                 load_full_patch <= (i != prev_i);
-//                mv = i - 1;
-//                nv = j - 1;
 
-                pixel_addr6 <= compute_addr(i-1, j+1);
-                pixel_addr7 <= compute_addr(i,   j+1);
-                pixel_addr8 <= compute_addr(i+1, j+1);
+                    pixel_addr6 <= compute_addr(i-1, j+1);
+                    pixel_addr7 <= compute_addr(i,   j+1);
+                    pixel_addr8 <= compute_addr(i+1, j+1);
 
 
                 if (load_full_patch)
@@ -66,7 +62,7 @@ module patch_addr_gen
                     pixel_addr2 <= compute_addr(i+1, j-1);
                     pixel_addr3 <= compute_addr(i-1, j);
                     pixel_addr4 <= compute_addr(i,   j);
-                    pixel_addr5 <= compute_addr(i+2, j);
+                    pixel_addr5 <= compute_addr(i+1, j);
                 end
                 
                 prev_i <= i;
@@ -74,22 +70,20 @@ module patch_addr_gen
             else
             begin
                 load_full_patch <= ((i << 1) != prev_i);
-//                mv = (i << 1) - 2;
-//                nv = (j << 1) - 2;
 
-                pixel_addr6 <= compute_addr((i << 1) - 2,   (j << 1) + 2);
-                pixel_addr7 <= compute_addr((i << 1), (j << 1) + 2);
-                pixel_addr8 <= compute_addr((i << 1) + 2, (j << 1) + 2);
+                    pixel_addr6 <= compute_addr((i << 1) - 2,   (j << 1) + 2);
+                    pixel_addr7 <= compute_addr((i << 1),       (j << 1) + 2);
+                    pixel_addr8 <= compute_addr((i << 1) + 2,   (j << 1) + 2);
 
                 if (load_full_patch)
                 begin
                     pixel_addr0 <= compute_addr((i << 1) - 2,   (j << 1) - 2);
-                    pixel_addr1 <= compute_addr((i << 1), (j << 1) - 2);
-                    pixel_addr2 <= compute_addr((i << 1) + 2, (j << 1) - 2);
+                    pixel_addr1 <= compute_addr((i << 1),       (j << 1) - 2);
+                    pixel_addr2 <= compute_addr((i << 1) + 2,   (j << 1) - 2);
                     
                     pixel_addr3 <= compute_addr((i << 1) - 2,   (j << 1));
-                    pixel_addr4 <= compute_addr((i << 1), (j << 1));
-                    pixel_addr5 <= compute_addr((i << 1) + 2, (j << 1));
+                    pixel_addr4 <= compute_addr((i << 1),       (j << 1));
+                    pixel_addr5 <= compute_addr((i << 1) + 2,   (j << 1));
                 end
                 
                 prev_i <= (i << 1);
--- src/conv/patch_data_latch.v
+++ src/conv/patch_data_latch.v
@@ -37,7 +37,7 @@ module patch_data_latch
 );
 
 
-    reg [2:0] state;
+    reg [3:0] state;
 
     always @(posedge clk or negedge rst) 
     begin
@@ -47,16 +47,6 @@ module patch_data_latch
             state <= 0;
             addr1 <= 0;
             addr2 <= 0;
-
-//            pixel0 <= 8'sd0;
-//            pixel1 <= 8'sd0;
-//            pixel2 <= 8'sd0;
-//            pixel3 <= 8'sd0;
-//            pixel4 <= 8'sd0;
-//            pixel5 <= 8'sd0;
-//            pixel6 <= 8'sd0;
-//            pixel7 <= 8'sd0;
-//            pixel8 <= 8'sd0;
         end 
         else if (load && !load_done)
         begin
@@ -81,44 +71,54 @@ module patch_data_latch
                         pixel4 <= pixel7;
                         pixel5 <= pixel8;
 
-                        state <= 4;
+                        state <= 7;
                         load_done <= 0;
                     end
                 end
 
-                1: begin // Read pixel0 and pixel1
+                1: state <= 2;
+
+                2: begin // Read pixel0 and pixel1
                     pixel0 <= data1;
                     pixel1 <= data2;
                     addr1 <= pixel_addr2;
                     addr2 <= pixel_addr3;
-                    state <= 2;
+                    state <= 3;
                 end
 
-                2: begin // Read pixel2 and pixel3
+                3: state <= 4;
+
+                4: begin // Read pixel2 and pixel3
                     pixel2 <= data1;
                     pixel3 <= data2;
                     addr1 <= pixel_addr4;
                     addr2 <= pixel_addr5;
-                    state <= 3;
+                    state <= 5;
                 end
 
-                3: begin // Read pixel4 and pixel5
+                5: state <= 6;
+
+                6: begin // Read pixel4 and pixel5
                     pixel4 <= data1;
                     pixel5 <= data2;
                     addr1 <= pixel_addr6;
                     addr2 <= pixel_addr7;
-                    state <= 4;
+                    state <= 7;
                 end
 
-                4: begin // Read pixel6 and pixel7
+                7: state <= 8;
+
+                8: begin // Read pixel6 and pixel7
                     pixel6 <= data1;
                     pixel7 <= data2;
                     addr1 <= pixel_addr8;
                     addr2 <= 10'd0;
-                    state <= 5;
+                    state <= 9;
                 end
 
-                5: begin // Read pixel8
+                9: state <= 10;
+
+                10: begin // Read pixel8
                     pixel8 <= data1;
                     load_done <= 1;
                     state <= 0;
